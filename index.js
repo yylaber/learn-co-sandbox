18e7de8b5306d08f2501acb03d5c3fe0738dbcee
Yosef Levertov <yylaber@gmail.com>
2018-06-29 18:08:06 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,10 @@
+function outerFunction() {
+  var innerVariable = "I'm sort of a secret.";
+  
+  return function innerScope() {
+    var inaccessible = "Nothing can touch me.";
+ 
+    return innerVariable;
+  }
+}
+var myScope = outerFunction();
